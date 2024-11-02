9e50165470ae27237ddaed8a7181288177ba8218
parth5757 <psthakkar2@gmail.com>
2024-11-02 13:38:33 +0530

    react signup profile pic work completed

--- Frontend/ecommerce/src/utils/Layout.js
+++ Frontend/ecommerce/src/utils/Layout.js
@@ -1,7 +1,11 @@
+import {ToastContainer} from 'react-toastify';
+
+
 const Layout=({children})=>{
     return(
       <>
         {children}
+        <ToastContainer position="bottom-right" autoclose={3000} highProgressBar={false} style={{marginBottom:'30px'}}/>
       </>
     )
   }
